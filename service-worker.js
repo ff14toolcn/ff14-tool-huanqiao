2a0e4596df07d28f9643d1776321f41ff3aee046
Etest2023 <etest177@126.com>
2023-10-05 03:00:59 +0800

    add service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(addResourcesToCache(['/', '/index.html']))
+})
+
+const addResourcesToCache = async (resources) => {
+    const cache = await caches.open('v1')
+    await cache.addAll(resources)
+}
+
+const cacheFirst = async (request) => {
+    const responseFromCache = await caches.match(request);
+    if (responseFromCache) return responseFromCache;
+    return fetch(request)
+}
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(cacheFirst(event.request))
+})
